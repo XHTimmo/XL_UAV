8210f2d17012afd481cb3ca478cd0423a28d50dc
XH_Timmo <slwhx2295317870@outlook.com>
2024-04-25 16:50:03 +0800

    添加 mpu6050 wake up

--- components/MPU6050/MPU6050.c
+++ components/MPU6050/MPU6050.c
@@ -87,6 +87,15 @@ static esp_err_t mpu6050_write(void *sensor, const uint8_t reg_start_addr, const
     return ret;
 }
 
+/***************************************************************************************************
+ *函数：MPU6050_handle mpu6050_create(i2c_port_t port, const uint16_t dev_addr)
+ *功能：创建MPU6050句柄
+ *参数：
+ *i2c_port_t port   MPU6050IIC端口
+ *const uint16_t dev_addr   MPU6050IIC地址
+ *返回值：指向MPU6050模块信息句柄的指针
+ *备注：创建包含了MPU6050模块信息的句柄
+ ***************************************************************************************************/
 static MPU6050_handle mpu6050_create(i2c_port_t port, const uint16_t dev_addr)
 {
     mpu6050_dev_t *sensor = (mpu6050_dev_t *)calloc(1, sizeof(mpu6050_dev_t));
@@ -98,12 +107,51 @@ static MPU6050_handle mpu6050_create(i2c_port_t port, const uint16_t dev_addr)
     return (MPU6050_handle)sensor;
 }
 
+/***************************************************************************************************
+ *函数：esp_err_t mpu6050_config(void *sensor, const mpu6050_acce_fs_t acce_fs, const mpu6050_gyro_fs_t gyro_fs)
+ *功能：配置MPU6050量程
+ *参数：
+ *void *sensor  指向MPU6050句柄的指针
+ *const mpu6050_acce_fs_t acce_fs   acce量程配置项
+ *const mpu6050_gyro_fs_t gyro_fs   gyro量程配置项
+ *返回值：错误值
+ *备注：
+ ***************************************************************************************************/
 static esp_err_t mpu6050_config(void *sensor, const mpu6050_acce_fs_t acce_fs, const mpu6050_gyro_fs_t gyro_fs)
 {
     uint8_t config_regs[2] = {gyro_fs << 3, acce_fs << 3};
     return mpu6050_write(sensor, MPU6050_GYRO_CONFIG, config_regs, sizeof(config_regs));
 }
 
+/***************************************************************************************************
+ *函数：esp_err_t mpu6050_wake_up(void *sensor)
+ *功能：唤醒MPU6050
+ *参数：
+ *void *sensor指向MPU6050句柄的指针
+ *返回值：无
+ *备注：
+ ***************************************************************************************************/
+static esp_err_t mpu6050_wake_up(void *sensor)
+{
+    esp_err_t ret;
+    uint8_t tmp;
+    ret = mpu6050_read(sensor, MPU6050_PWR_MGMT_1, &tmp, 1);
+    if (ESP_OK != ret)
+    {
+        return ret;
+    }
+    tmp &= (~BIT6);
+    ret = mpu6050_write(sensor, MPU6050_PWR_MGMT_1, &tmp, 1);
+    return ret;
+}
+
+/***************************************************************************************************
+ *函数：void i2c_sensor_mpu6050_init(void)
+ *功能：初始化MPU6050
+ *参数：
+ *返回值：无
+ *备注：包含创建MPU6050模块、配置量程、唤醒模块、获取零漂
+ ***************************************************************************************************/
 void i2c_sensor_mpu6050_init(void)
 {
     esp_err_t ret;
@@ -111,24 +159,33 @@ void i2c_sensor_mpu6050_init(void)
     mpu6050 = mpu6050_create(I2C_MASTER_NUM, MPU6050_IIC_ADDR);
     if (mpu6050 != NULL)
     {
-        ESP_LOGI(TAG, "successful creat a mpu6050 handle");
+        ESP_LOGI(TAG, "1 successful creat a mpu6050 handle");
     }
     else
     {
-        ESP_LOGE(TAG, "fail creat a mpu6050 handle");
+        ESP_LOGE(TAG, "1 fail creat a mpu6050 handle");
     }
 
     ret = mpu6050_config(mpu6050, ACCE_FS_4G, GYRO_FS_500DPS);
     if (ret != ESP_FAIL)
     {
-        ESP_LOGI(TAG, "successful config mpu6050");
-        ESP_LOGI(TAG,"ACCE_FS_%d,GYRO_FS_%dDPS",(int)(pow(2,(ACCE_FS_4G+1))),(int)(250*pow(2,(ACCE_FS_4G))));
+        ESP_LOGI(TAG, "2 successful config mpu6050");
+        ESP_LOGI(TAG, "  ACCE_FS_%d,GYRO_FS_%dDPS", (int)(pow(2, (ACCE_FS_4G + 1))), (int)(250 * pow(2, (ACCE_FS_4G))));
     }
     else
     {
-        ESP_LOGE(TAG, "fail to config mpu6050");
+        ESP_LOGE(TAG, "2 fail to config mpu6050");
     }
+    TEST_ASSERT_EQUAL(ESP_OK, ret);
 
-    
+    ret = mpu6050_wake_up(mpu6050);
+    if (ret != ESP_FAIL)
+    {
+        ESP_LOGI(TAG, "3 successful wake up mpu6050");
+    }
+    else
+    {
+        ESP_LOGE(TAG, "3 fail to wake up mpu6050");
+    }
     TEST_ASSERT_EQUAL(ESP_OK, ret);
 }
