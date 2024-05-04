84c0d8ada6ffa72a61d153495487afb62c274079
XH_Timmo <slwhx2295317870@outlook.com>
2024-05-04 15:17:00 +0800

    移植蓝牙模块

--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
 #include <BMP280.h>
 #include "esp_log.h"
 #include <MOTOR.h>
+#include "../components/BLU/include/BLU.h"
 #include "driver/gpio.h"
 complimentary_angle_t complimentary_angle = {
     .pitch = 0,
@@ -25,16 +26,16 @@ void app_main(void)
 {
     showbanner();
     MainInit();
-    vTaskDelay(2000/portTICK_PERIOD_MS);
-    while (1)
-    {
-        IMU(&complimentary_angle);
-        ESP_LOGI("main","pitch:%10.2f roll:%10.2f accz:%10.2f ", complimentary_angle.pitch, complimentary_angle.roll, complimentary_angle.accz);
-        // mpu6050_raw_data_return_t mpu6050;
-        // mpu6050 = mpu6050_get_raw_data();
-        // printf("accx %5.2lf accy %5.2lf accz %5.2lf gyrox %5.2lf gyroy %5.2lf gyroz %5.2lf\n",mpu6050.x1,mpu6050.y1,mpu6050.z1,mpu6050.x2,mpu6050.y2,mpu6050.z2);
-        // printf("%5.2f\n",mpu6050.x1);
-        vTaskDelay(10/portTICK_PERIOD_MS);
-    }
+    BLU_init();
+    // while (1)
+    // {
+    //     IMU(&complimentary_angle);
+    //     ESP_LOGI("main","pitch:%10.2f roll:%10.2f accz:%10.2f ", complimentary_angle.pitch, complimentary_angle.roll, complimentary_angle.accz);
+    //     // mpu6050_raw_data_return_t mpu6050;
+    //     // mpu6050 = mpu6050_get_raw_data();
+    //     // printf("accx %5.2lf accy %5.2lf accz %5.2lf gyrox %5.2lf gyroy %5.2lf gyroz %5.2lf\n",mpu6050.x1,mpu6050.y1,mpu6050.z1,mpu6050.x2,mpu6050.y2,mpu6050.z2);
+    //     // printf("%5.2f\n",mpu6050.x1);
+    //     vTaskDelay(10/portTICK_PERIOD_MS);
+    // }
     
 }
